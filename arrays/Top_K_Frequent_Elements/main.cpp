b362ad205f2b949fe701b3f7934a346e6c622fc5
Sergey Kosykh <s3rkosal@gmail.com>
2026-06-13 17:10:27 +0400

    added cpp top k bucket sort

--- arrays/Top_K_Frequent_Elements/main.cpp
+++ arrays/Top_K_Frequent_Elements/main.cpp
@@ -24,6 +24,33 @@ vector<int> topKFrequent(const vector<int>& nums, int k) {
     return res;
 }
 
+vector<int> topKFrequentBucketSort(const vector<int>& nums, int k) {
+
+    vector<int> res = {}; res.reserve(k);
+
+    // O(n)
+    map<int, int> freq;
+    for (int num: nums) 
+        ++freq[num];
+
+    // O(n)
+    vector<vector<int>> buckets(nums.size());
+    for (auto [num, fr]: freq)
+        buckets[fr-1].push_back(num);
+
+    // O(k)
+    for (
+        auto [it, i] = tuple(buckets.rbegin(), 0); 
+        it != buckets.rend(); 
+        ++it
+    ) for (int num : *it) {
+        res.push_back(num);
+        if (++i >= k) return res;
+    }
+
+    return res;
+}
+
 int main() {
 
     vector<pair<vector<int>, int>> vecs = {
@@ -34,7 +61,7 @@ int main() {
     };
 
     for (const auto& [vec, k]: vecs) 
-        println("{}", topKFrequent(vec, k));
+        println("{}", topKFrequentBucketSort(vec, k));
 
     return 0;
 }

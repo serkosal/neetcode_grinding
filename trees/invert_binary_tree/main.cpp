0b750d016f29bc71c03b9cede7832556f22ee059
Sergey Kosykh <s3rkosal@gmail.com>
2026-06-16 15:09:03 +0400

    added cpp solution to the problem "Invert Binary Tree"

--- trees/invert_binary_tree/main.cpp
+++ trees/invert_binary_tree/main.cpp
@@ -1,9 +1,25 @@
 #include "cpp_helpers/helpers.hpp"
 #include "cpp_helpers/treeNode.hpp"
 
+TreeNode* invertTree(TreeNode* const root) {
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        auto cur = q.front(); q.pop();
+        if (!cur) continue;
+
+        std::swap(cur->left, cur->right);
+        q.push(cur->left); q.push(cur->right);
+    };
+
+    return root;
+}
+
 int main() {
 
-    TreeNode root(0, 
+    auto root = new TreeNode{0, 
         new TreeNode{1,
             new TreeNode{3},
             new TreeNode{4}, 
@@ -11,9 +27,10 @@ int main() {
         new TreeNode{2,
             new TreeNode{5}
         }
-    );
+    };
+    invertTree(root);
 
-    cout << root;
+    cout << *root;
 
     return 0;
 }
